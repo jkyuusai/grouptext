f8cc53e0916f95738239ba8f471c398d44766c63
Joshua Skinner <jkyuusai@gmail.com>
2014-08-26 21:17:00 -0400

    Remove search from header and some cleanup

--- client/contacts/addContact.js
+++ client/contacts/addContact.js
@@ -1,18 +1,17 @@
 Template.addContact.events({
-  'submit form': function(e) {
+  'submit form': function(e, template) {
     e.preventDefault();
     Session.set('unsubscribed', false);
     var contact = {
-      name: $(e.target).find('[name=name]').val(),
-      number: $(e.target).find('[name=phone]').val(),
-      carrier:  $(e.target).find('[name=carrier]').val()
+      name: template.$('[name=name]').val(),
+      number: template.$('[name=phone]').val(),
+      carrier:  template.$('[name=carrier]').val()
     }
 
     Meteor.call('addContact', contact, function (error, id) {
        if (error) {                  
         alert(error.reason);
-      } else {
-          console.log('contact added',Contacts.findOne(id));          
+      } else {                  
           //Meteor.call('sendEmail', Contacts.findOne(id));
           Router.go('contactList');       
       }
