95e6bc03e6bfd442530a81541ead700ce842a756
Joshua Skinner <jkyuusai@gmail.com>
2014-09-01 12:33:55 -0400

    Support more carriers

--- server/server.js
+++ server/server.js
@@ -6,12 +6,18 @@ Meteor.publish('contacts', function(userId) {
 	return Contacts.find( {userId:userId} );
 })
 
-//TODO: Change this to a collection
+//TODO: Change this to a collection? Unsure if there's a benefit to doing so.
 var carrierList = {
-			'AT&T': 'mms.att.net',
-			'Verizon': 'vtext.com',
-			'TMobile': 'tmomail.net',
-			'Sprint': 'messaging.sprintpcs.com'
+	'AT&T': 'mms.att.net',
+	'Boost': 'myboostmobile.com',
+	'MetroPCS': 'mymetropcs.com',
+	'Sprint': 'messaging.sprintpcs.com',
+	'Straight Talk (AT&T)': 'mms.att.net',			
+	'Straight Talk (VZW)': 'vtext.com',			
+	'TMobile': 'tmomail.net',			
+	'Ting': 'message.ting.com',
+	'Verizon': 'vtext.com',
+	'Virgin Mobile': 'vmpix.com'
 }
 
 Meteor.methods({
